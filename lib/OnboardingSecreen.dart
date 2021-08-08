e5f833bd881aff5802c8fb6f7887ac8f2ec03403
baali-boudjemaa <baaliboudjemaaens@gmail.com>
2021-08-08 11:02:58 -0700

    add OnboardingSecreen

---
+++ lib/OnboardingSecreen.dart
@@ -0,0 +1,19 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+
+class OnboardingSecreen extends StatefulWidget{
+  @override
+  _OnboardingSecreen createState() =>_OnboardingSecreen();
+
+
+}
+class _OnboardingSecreen extends State<OnboardingSecreen>{
+  @override
+  Widget build(BuildContext context) {
+
+    throw UnimplementedError();
+  }
+
+}
